61fbd43b5636d92fe54bc541b2f804a51dbd6811
Artur Kosałka <kosial@tlen.pl>
2018-10-14 22:45:16 +0200

    Adding PRODUKT column

---
+++ Projekt01DB/Projekt01DB/dbo/Tables/PRODUKT.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[PRODUKT] (
+    [ID]              INT             IDENTITY (1, 1) NOT NULL,
+    [NumerKatalogowy] INT             NULL,
+    [Nazwa]           VARCHAR (50)    NOT NULL,
+    [Rocznik]         DATE            NULL,
+    [Kolor]           VARCHAR (50)    NULL,
+    [Przebieg]        NUMERIC (20, 2) NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [ID_Producent_NumerKatalogowy] FOREIGN KEY ([NumerKatalogowy]) REFERENCES [dbo].[PRODUCENT] ([ID]) ON DELETE SET NULL ON UPDATE SET NULL
+);
+
