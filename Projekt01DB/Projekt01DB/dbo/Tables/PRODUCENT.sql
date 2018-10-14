dba04729e5c52deb43f58c21716b96ab4b624fcc
Artur Kosałka <kosial@tlen.pl>
2018-10-14 22:43:47 +0200

    Adding PRODUCENT column

---
+++ Projekt01DB/Projekt01DB/dbo/Tables/PRODUCENT.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[PRODUCENT] (
+    [ID]               INT           IDENTITY (1, 1) NOT NULL,
+    [Marka]            VARCHAR (20)  NOT NULL,
+    [PelnaNazwa]       VARCHAR (100) NULL,
+    [DataZalozenia]    DATE          NULL,
+    [Panstwo]          VARCHAR (20)  NOT NULL,
+    [IloscPracownikow] NUMERIC (18)  NOT NULL,
+    [Wartosc]          MONEY         NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
